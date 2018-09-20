e9c8f89cbdfd9c220003cb21ad5bee9b345e61f0
Mochammad Iqbal <mochiqbal29@gmail.com>
2018-09-20 16:46:31 +0700

    Merge

---
+++ app/src/main/java/qblmchmmd/com/cryptracker/viewmodel/MainViewModel.kt
@@ -0,0 +1,48 @@
+package qblmchmmd.com.cryptracker.viewmodel
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
+import qblmchmmd.com.cryptracker.repository.Repository
+import qblmchmmd.com.cryptracker.model.CryptoListResponse
+import java.io.IOException
+import kotlin.coroutines.experimental.CoroutineContext
+
+class MainViewModel(private val repository: Repository<CryptoListResponse>,
+                    private val mainThread : CoroutineContext,
+                    private val bgThread : CoroutineContext):ViewModel() {
+
+    private val data = MutableLiveData<String>()
+    val uiData : LiveData<String> = Transformations.map(data) {
+        it
+    }
+
+    private val loading = MutableLiveData<Boolean>()
+    val isLoading : LiveData<Boolean> = Transformations.map(loading) {
+        it
+    }
+
+    private val error = MutableLiveData<Boolean>()
+    val isError : LiveData<Boolean> = Transformations.map(error) {
+        it
+    }
+
+    fun update() {
+        GlobalScope.launch(mainThread){
+            loading.value = true
+            try {
+                data.value = withContext(bgThread) { repository.getData().toString() }
+            } catch (err : Exception) {
+//                Log.d(this::class.java.simpleName, err.localizedMessage)
+                error.value = true
+            }
+            loading.value = false
+        }
+    }
+
+}
