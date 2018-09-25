b4d08416c5d99f298675caf465de146c869a45e7
Mochammad Iqbal <mochiqbal29@gmail.com>
2018-09-25 12:05:34 +0700

    Use mockk on CryptoRepositoryUnitTest

--- app/src/test/java/qblmchmmd/com/cryptracker/CryptoRepositoryUnitTest.kt
+++ app/src/test/java/qblmchmmd/com/cryptracker/CryptoRepositoryUnitTest.kt
@@ -1,12 +1,20 @@
 package qblmchmmd.com.cryptracker
 
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Assert
 import org.junit.Test
 import mock.LocalDataMock
 import mock.RemoteDataMock
+import mock.mockData
 import mock.mockDataNull
+import qblmchmmd.com.cryptracker.datasource.local.LocalData
+import qblmchmmd.com.cryptracker.datasource.remote.RemoteData
 import qblmchmmd.com.cryptracker.model.CryptoListResponse
 import qblmchmmd.com.cryptracker.repository.CryptoRepository
 
@@ -15,15 +23,18 @@ class CryptoRepositoryUnitTest {
     @Test
     fun whenGetData_remoteFetchDataCalled() {
 
-        val remoteData = RemoteDataMock(mockDataNull)
-        val localData = LocalDataMock<CryptoListResponse>()
+        // Given
+        val remoteData = mockk<RemoteData<CryptoListResponse>>()
+        val localData = mockk<LocalData<CryptoListResponse>>()
+        coEvery { remoteData.fetchData() } returns GlobalScope.async { mockData }
         val cryptoRepo = CryptoRepository(remoteData = remoteData, localData = localData)
 
+        // When
         runBlocking {
             cryptoRepo.getData()
-            delay(1000)
         }
 
-        Assert.assertEquals(remoteData.fetchDataCalled, true)
+        // Then
+        coVerify { remoteData.fetchData() }
     }
 }
